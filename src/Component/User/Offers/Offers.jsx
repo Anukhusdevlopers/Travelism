3266f3e7ba2f28cf01282b1c55f6467f4cec00e0
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2025-03-01 22:17:03 +0530

    useles changes

--- src/Component/User/Offers/Offers.jsx
+++ src/Component/User/Offers/Offers.jsx
@@ -10,21 +10,21 @@ const Offers = () => {
       title: 'Bali Beach Getaway',
       description: 'Enjoy 5 nights at a luxury resort with complimentary breakfast and spa treatment.',
       price: '$899',
-      image: 'https://riyaztourandtravels.com/about/01.webp'
+      image: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D'
     },
     {
       id: 2,
       title: 'European Adventure',
       description: 'Visit 4 countries in 10 days with guided tours and premium accommodations.',
       price: '$1,299',
-      image: 'https://riyaztourandtravels.com/slider/slider-2.jpg'
+      image: 'https://images.unsplash.com/photo-1519677100203-a0e668c92439?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8RVVST1BFQU4lMjBhZHZlbnR1ZXxlbnwwfHwwfHx8MA%3D%3D'
     },
     {
       id: 3,
       title: 'Mountain Retreat',
       description: 'Escape to the mountains with this all-inclusive 3-night package.',
       price: '$599',
-      image: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80'
+      image: 'https://images.unsplash.com/photo-1465056836041-7f43ac27dcb5?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
   ];
   
@@ -53,7 +53,6 @@ const Offers = () => {
         ))}
       </div>
       
-      <button className={styles.viewAllButton}><Link to={'/offer'} >View All Offers</Link></button>
     </section>
   );
 }
